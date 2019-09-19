2c01e677435f8dacfbdf54168c8f41bbe75019ab
Jill Headen <jheaden@gmail.com>
2019-09-19 10:22:50 -0700

    Init git commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fbe = new FizzBuzzEvaluator();
+
+            for (int x = 1; x<= 100; x++)
+            {
+                Console.WriteLine(fbe.Evaluate(x));
+            }
+
+
+        }
+    }
+}
